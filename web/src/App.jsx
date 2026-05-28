51b99fda3eceaa46200f6d0cd533dcdbf2f64215
Ashomondi <adhiamboa745@gmail.com>
2026-05-28 23:06:24 +0300

    [200~feat: add React Router setup with top and bottom navigation~

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,1 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import VerifierDashboard from './pages/VerifierDashboard';
+import IssuerDashboard from './pages/IssuerDashboard';
+import { TopAppBar } from './components/ui/TopAppBar';
+import { BottomNavBar } from './components/ui/BottomNavBar';
 
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-surface flex flex-col">
+        <TopAppBar />
+        <main className="flex-1 pb-24 pt-16">
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/verify" element={<VerifierDashboard />} />
+            <Route path="/issue" element={<IssuerDashboard />} />
+          </Routes>
+        </main>
+        <BottomNavBar />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
